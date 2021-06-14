c1d1bbfe9cccef8cb88b0e470f7175ff7d9be68e
Killian Meunier <killian.meunier@etu.uca.fr>
2021-06-14 20:42:02 +0200

    implementation de l'animation qui sert d'exemple

--- animation/src/animation.c
+++ animation/src/animation.c
@@ -1,18 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+void end_sdl(char ok,                                                 // fin normale : ok = 0 ; anormale ok = 1
+                  char const* msg,                                    // message à afficher
+                  SDL_Window* window,                                 // fenêtre à fermer
+                  SDL_Renderer* renderer) {                           // renderer à fermer
+  char msg_formated[255];                                         
+  int l;                                                          
+
+  if (!ok) {                                                      
+         strncpy(msg_formated, msg, 250);                                 
+         l = strlen(msg_formated);                                        
+         strcpy(msg_formated + l, " : %s\n");                     
+
+         SDL_Log(msg_formated, SDL_GetError());                   
+  }                                                               
+
+  if (renderer != NULL) SDL_DestroyRenderer(renderer);                            
+  if (window != NULL)   SDL_DestroyWindow(window);                                        
+
+  SDL_Quit();                                                     
+
+  if (!ok) {                                                      
+         exit(EXIT_FAILURE);                                              
+  }                                                               
+}
+
+void play_with_texture_3(SDL_Texture* my_texture,
+                         SDL_Window* window,
+                         SDL_Renderer* renderer) {
+  SDL_Rect 
+        source = {0},                             // Rectangle définissant la zone de la texture à récupérer
+        window_dimensions = {0},                  // Rectangle définissant la fenêtre, on n'utilisera que largeur et hauteur
+        destination = {0};                        // Rectangle définissant où la zone_source doit être déposée dans le renderer
+
+  SDL_GetWindowSize(                                
+      window, &window_dimensions.w,                 
+      &window_dimensions.h);                      // Récupération des dimensions de la fenêtre
+  SDL_QueryTexture(my_texture, NULL, NULL,         
+                   &source.w,                       
+                   &source.h);                    // Récupération des dimensions de l'image
+
+  /* On décide de déplacer dans la fenêtre         cette image */
+  float zoom = 0.25;                              // Facteur de zoom entre l'image source et l'image affichée
+
+  int nb_it = 200;                                // Nombre d'images de l'animation
+  destination.w = source.w * zoom;                // On applique le zoom sur la largeur
+  destination.h = source.h * zoom;                // On applique le zoom sur la hauteur
+  destination.x =                                   
+      (window_dimensions.w - destination.w) / 2;  // On centre en largeur
+  float h = window_dimensions.h - destination.h;  // hauteur du déplacement à effectuer
+
+  for (int i = 0; i < nb_it; ++i) {
+    destination.y =
+        h * (1 - exp(-5.0 * i / nb_it) / 2 *
+                     (1 + cos(10.0 * i / nb_it * 2 *
+                              M_PI)));            // hauteur en fonction du numéro d'image
+
+    SDL_RenderClear(renderer);                    // Effacer l'image précédente
+
+    SDL_SetTextureAlphaMod(my_texture,(1.0-1.0*i/nb_it)*255);      // L'opacité va passer de 255 à 0 au fil de l'animation
+    SDL_RenderCopy(renderer, my_texture, &source, &destination);   // Préparation de l'affichage
+    SDL_RenderPresent(renderer);                  // Affichage de la nouvelle image
+    SDL_Delay(30);                                // Pause en ms
+  }                                                 
+  SDL_RenderClear(renderer);                      // Effacer la fenêtre une fois le travail terminé
+}
 
 int main(int argc, char **argv) 
 {
     (void)argc;
     (void)argv;
 
-    /* Initialisation de la SDL  + gestion de l'échec possible */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_Log("Error : SDL initialisation - %s\n", SDL_GetError());      // l'initialisation de la SDL a échoué 
-        exit(EXIT_FAILURE);
-    }
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_DisplayMode screen;
 
-    SDL_Quit();
-    return 0;
+    /* INITIALISATIONS */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) end_sdl(0, "ERROR SDL INIT", window, renderer);
+
+    SDL_GetCurrentDisplayMode(0, &screen);
+     window = SDL_CreateWindow("Premier dessin",
+                            SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, screen.w * 0.66,
+                            screen.h * 0.66,
+                            SDL_WINDOW_OPENGL);
+    if (window == NULL) end_sdl(0, "ERROR WINDOW CREATION", window, renderer);
+
+    renderer = SDL_CreateRenderer(
+           window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL) end_sdl(0, "ERROR RENDERER CREATION", window, renderer);
+
+    /*TRAITEMENT*/
+    SDL_Texture * my_texture; 
+    my_texture = IMG_LoadTexture(renderer,"../img/Maze.png");
+    if (my_texture == NULL) end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
+
+    play_with_texture_3(my_texture, window, renderer);
+
+    IMG_Quit();
+    SDL_DestroyTexture(my_texture); 
+    end_sdl(1, "Normal ending", window, renderer);
+    return EXIT_SUCCESS;
 }
