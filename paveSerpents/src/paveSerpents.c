df78f3557db0a128036032299f7123addd3fdb6b
Lilian <lilian@LilianDebianVM>
2021-06-14 17:09:34 +0200

    Updated .h and changed file names

--- paveSerpents/src/paveSerpents.c
+++ paveSerpents/src/paveSerpents.c
@@ -1,6 +1,6 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
-#include "panierSerpents.h"
+#include "paveSerpents.h"
 #include "bool.h"
 
 
---
+++ paveSerpents/src/paveSerpents.h
@@ -0,0 +1,10 @@
+#include "bool.h"
+
+#ifndef PAVE_SERPENTS_H
+#define PAVE_SERPENTS_H
+
+#define WIN_WIDTH 960
+#define WIN_HEIGHT 540
+#define WIN_TITLE "Pavé de serpents"
+
+#endif
