10c4e706b83d685bbad7a42cf6915f511dee6c3d
machandeze <marius.chandeze@etu.uca.fr>
2021-06-16 23:39:26 +0000

    corrections, implementation de areInScreen

--- mXfenetre/src/xfenetre.c
+++ mXfenetre/src/xfenetre.c
@@ -18,17 +18,34 @@ int main(int argc, char **argv) {
 
 	return 0;
 }
+#include <stdio.h>
+bool areInScreen(int x[], int y[], SDL_DisplayMode screen) {
+	int i = 0;
+	bool code = true;
+	
+	while(i < NB && code == true) {
+		if(screen.w <= x[i] || x[i] < 0 || screen.h <= y[i] || y[i] < 0) {
+			code = false;
+			puts("fin de l'ecran");
+		}
+		++i;
+	}
+	return code;
+}
 
 void moveWindows(SDL_Window *windows[], int delay) {
-	int x[NB], y[NB], swidth, sheight, i, width[NB], height[NB];
+	int x[NB], y[NB], i, width[NB], height[NB];
+	SDL_DisplayMode screen;
 	
+	SDL_GetCurrentDisplayMode(0, &screen);
 	for(i = 0; i < NB; ++i) {
-		SDL_GetWindowPosition(windows[i], &x[i], &y[i]);
 		SDL_GetWindowSize(windows[i], &width[i], &height[i]);
-		SDL_SetWindowPosition(windows[i], swidth / 2, sheight / 2);
+		SDL_SetWindowPosition(windows[i], screen.w / 2, screen.h / 2);
+		SDL_GetWindowPosition(windows[i], &x[i], &y[i]);
 	}
-	SDL_Delay(1000);
-	for(i = 0; i < 50; ++i) {
+	SDL_Delay(1500);
+	i = 0;
+	while(areInScreen(x, y, screen)) {
 		SDL_SetWindowPosition(windows[0], x[0] + i, y[0] - i);
 		SDL_GetWindowPosition(windows[0], &x[0], &y[0]);
 		
@@ -42,7 +59,9 @@ void moveWindows(SDL_Window *windows[], int delay) {
 		SDL_GetWindowPosition(windows[3], &x[3], &y[3]);
 		
 		SDL_Delay(delay);
+		++i;
 	}
+	SDL_Delay(1000);
 }
 
 void createWindows(SDL_Window *windows[]) {
