fa192ae693e48f7c3077214c96d382f98785778e
machandeze <marius.chandeze@etu.uca.fr>
2021-06-16 23:23:29 +0000

    ajout du type bool et de la fonction areInScreen

--- mXfenetre/src/xfenetre.h
+++ mXfenetre/src/xfenetre.h
@@ -4,6 +4,9 @@
 
 #define NB 4
 
+typedef enum { false, true } bool;
+
 void moveWindows(SDL_Window *windows[], int delay);
 void createWindows(SDL_Window *windows[]);
 void destroyWindows(SDL_Window *windows[]);
+bool areInScreen(SDL_Window *windows[], SDL_DisplayMode screen);
