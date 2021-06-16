4128bf15d0fbb800bff0bb9316c35676f48d6b94
machandeze <marius.chandeze@etu.uca.fr>
2021-06-16 23:57:27 +0000

    creation de paveSerpent.c

---
+++ mPaveSerpents/src/paveSerpents.c
@@ -0,0 +1,10 @@
+#include "paveSerpents.h"
+
+int main(int argc, char **argv) {
+	(void)argc;
+	(void)argv;
+}
+
+void moveSquare1(SDL_Renderer *renderer);
+void moveSquare2(SDL_Renderer *renderer);
+void moveSquare3(SDL_Renderer *renderer);
