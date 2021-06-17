8053cc5b3a47f94c5cf497d67828afbcd70f8300
machandeze <machandeze@turing.local.isima.fr>
2021-06-17 09:56:25 +0200

    initialisation de la fenetre et du renderer

--- mAnimation/src/animation.c
+++ mAnimation/src/animation.c
@@ -4,6 +4,7 @@ int main(int argc, char **argv) {
 	SDL_Renderer *renderer;
 	SDL_Window *window;
 	SDL_Texture *texture;
+	int x = 100, y = 100, height = 300, width = 500, delay = 100;
 	
 	(void)argc;
 	(void)argv;	
@@ -18,6 +19,24 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 	
+	window = SDL_CreateWindow("animation", x, y, width, height, 0);
+	if(window != NULL) {
+		fputs("erreur creation de la fenetre\n", stderr);
+		IMG_Quit();
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if(renderer != NULL) {
+		fputs("erreur creation du renderer\n", stderr);
+		SDL_DestroyWindow(window);
+		IMG_Quit();
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+	
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	IMG_Quit();
 	SDL_Quit();
 	
