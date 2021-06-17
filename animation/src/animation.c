3bbff517bc6d28c232adbf25e7e04c45e7d51e0f
Lilian <lilian@LilianDebianVM>
2021-06-17 10:59:45 +0200

    Added character jump

--- animation/src/animation.c
+++ animation/src/animation.c
@@ -49,8 +49,6 @@ SDL_Texture* loadTexture(char  *  file_image_name, SDL_Window *window, SDL_Rende
 }
 
 void showTiles(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* background) {
-	background = loadTexture("dirt_grass.png", window, renderer);
-
 	SDL_Rect source = {0},                      // Rectangle définissant la zone de la texture à récupérer
            window_dimensions = {0},           // Rectangle définissant la fenêtre, on n'utilisera que largeur et hauteur
            destination = {0};    
@@ -80,12 +78,75 @@ void showTiles(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* backgrou
     	destination.y = (window_dimensions.h - destination.h) - 2*source.h;  // La destination est au milieu de la hauteur de la fenêtre
 		SDL_RenderCopy(renderer, background, &source, &destination);
 	}
+	destination.w = source.w * zoom;         // La destination est un zoom de la source
+	destination.h = source.h * zoom;         // La destination est un zoom de la source
+	destination.x = (window_dimensions.w - destination.w) - 4*source.w;     // La destination est au milieu de la largeur de la fenêtre
+	destination.y = (window_dimensions.h - destination.h) - 1*source.h;  // La destination est au milieu de la hauteur de la fenêtre
+
+	SDL_RenderCopy(renderer, background, &source, &destination);
+}
+
+void animation(SDL_Window * window, SDL_Renderer * renderer, SDL_Texture * character, SDL_Texture* ground) {
+  SDL_Rect    character_source = {0},                // Rectangle définissant la zone totale de la planche
+			  ground_source = {0},
+              window_dimensions = {0},     // Rectangle définissant la fenêtre, on n'utilisera que largeur et hauteur
+              destination = {0},           // Rectangle définissant où la zone_source doit être déposée dans le renderer
+              state = {0};                 // Rectangle de la vignette en cours dans la planche 
+  int         offset_y,
+              offset_x,
+              x,
+              speed = 15, 
+              nb_images = 9;               // 9 Images sur la premiere ligne de la planche     
+  float       zoom = 2;  
+
+  character = loadTexture("player_tilesheet.png", window, renderer);
+  ground = loadTexture("dirt_grass.png", window, renderer);
+
+  SDL_GetWindowSize(window, &window_dimensions.w, &window_dimensions.h);
+  SDL_QueryTexture(character, NULL, NULL, &character_source.w, &character_source.h);  
+  SDL_QueryTexture(ground, NULL, NULL, &ground_source.w, &ground_source.h);
+
+  // Dimensions de la vignette dans la planche
+  offset_x = character_source.w / nb_images, 
+  offset_y = character_source.h / 3;
+  // Dimensions de la vignette
+  state.w = offset_x;             
+  state.h = offset_y;             
+  // Dimensions du sprite a l'ecran
+  destination.w = offset_x * zoom;       
+  destination.h = offset_y * zoom;     
+  // Position du debut de l'animation 
+  destination.y = window_dimensions.h - offset_y*zoom - ground_source.h;
+
+  for (x = 0; x < window_dimensions.w - destination.w; x += speed) {
+    destination.x = x;              // Position x de la vignette (elle bouge horizontalement)
+	if (x > (window_dimensions.w - destination.w)/2 && x < (window_dimensions.w - destination.w)/2+speed){
+		destination.y -= ground_source.h;
+	}
+	if (x > (window_dimensions.w - destination.w)/2+9*speed && x < (window_dimensions.w - destination.w)/2+10*speed){
+		destination.y -= ground_source.h;
+	}	
+    state.x += offset_x;            // Vignette suivante
+    state.x %= character_source.w;            // Pour revenir a la vignette du debut        
+    
+    SDL_RenderClear(renderer);      
+    //placerImage(fond, window, renderer, 3.5, 0 - x*0.8, - window_dimensions.w/2);
+    //placerImage(sol, window, renderer, 4, 0 - x * 1.5, window_dimensions.h - 150 );
+	showTiles(window, renderer, ground);
+    SDL_RenderCopy(renderer, character, &state, &destination);  
+    SDL_RenderPresent(renderer);        
+    SDL_Delay(80);              
+  }
+  SDL_RenderClear(renderer);       
 }
 
 int main(int argc, char const *argv[]) {
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
-	SDL_Texture* background = NULL;
+	SDL_Texture* ground = NULL;
+	SDL_Texture* character = NULL;
+	SDL_Texture* sky1 = NULL;
+	SDL_Texture* sky2 = NULL;
 	
 	bool_t status;
 
@@ -107,16 +168,14 @@ int main(int argc, char const *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	
-	showTiles(window, renderer, background);
+	animation(window, renderer, ground, character, sky1, sky2);
 	
 	SDL_RenderPresent(renderer);  
 
 	//createWindows(windows, nbWindows);
-	SDL_Delay(1000);
 	//moveWindows(windows, nbWindows);
-	SDL_Delay(1000);
 
-	SDL_DestroyTexture(background);
+	SDL_DestroyTexture(ground);
 	IMG_Quit();
 	quitSDL(true, "SDL END", window, renderer);
 
