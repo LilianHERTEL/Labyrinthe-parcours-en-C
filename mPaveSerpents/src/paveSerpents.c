52477f8db0c2f1eea6b8a4d89ecf8e06b7fabac8
machandeze <marius.chandeze@etu.uca.fr>
2021-06-17 00:14:52 +0000

    implementation de la structure du programme principal

--- mPaveSerpents/src/paveSerpents.c
+++ mPaveSerpents/src/paveSerpents.c
@@ -1,8 +1,38 @@
 #include "paveSerpents.h"
 
 int main(int argc, char **argv) {
+	SDL_Renderer *renderer;
+	SDL_Window *window;
+	int i, x = 100, y = 100, width = 500, height = 300;
+
 	(void)argc;
 	(void)argv;
+	
+	if(!SDL_Init()) {
+		return EXIT_FAILURE;
+	}
+	window = SDL_CreateWindow("pave de serpents", 100, 100, 500, 300, 0);
+	if(window == NULL) {
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+	renderer = SDL_CreateRenderer(window, -1 ,SDL_RENDERER_ACCELERATED);
+	if(renderer == NULL) {
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+	
+	for(i = 0; i < 1000; ++i) {
+		moveSquare1(renderer);
+		moveSquare2(renderer);
+		moveSquare3(renderer);
+		SDL_RenderPresent(renderer);
+	}
+	
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 void moveSquare1(SDL_Renderer *renderer);
