e3c1e685b17d182144705ca7fc833a02b3657e93
machandeze <machandeze@turing.local.isima.fr>
2021-06-17 09:46:56 +0200

    initialisation de la SDL et de SDL_Image

--- mAnimation/src/animation.c
+++ mAnimation/src/animation.c
@@ -1,1 +1,29 @@
 #include "animation.h"
+
+int main(int argc, char **argv) {
+	SDL_Renderer *renderer;
+	SDL_Window *window;
+	SDL_Texture *texture;
+	
+	(void)argc;
+	(void)argv;	
+	if(SDL_Init(0)) {
+		fputs("erreur chargement de la SDL\n", stderr);
+		return EXIT_FAILURE;
+	}
+	
+	if(IMG_Init(0)) {
+		fputs("erreur chargement de SDL_Image\n", stderr);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+	
+	IMG_Quit();
+	SDL_Quit();
+	
+	return 0;
+}
+
+SDL_Texture* loadTextureFromImage(char *name, SDL_Renderer *renderer);
+void displayFrame(SDL_Renderer *renderer, int frame);
+
--- mAnimation/src/animation.h
+++ mAnimation/src/animation.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 SDL_Texture* loadTextureFromImage(char *name, SDL_Renderer *renderer);
 void displayFrame(SDL_Renderer *renderer, int frame);
