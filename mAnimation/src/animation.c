b39f1683018e6ddeba797771087f70d5cd2154dc
machandeze <machandeze@turing.local.isima.fr>
2021-06-17 12:04:54 +0200

    implementation du tirage des cartes et de son affichage

--- mAnimation/src/animation.c
+++ mAnimation/src/animation.c
@@ -4,16 +4,16 @@ int main(int argc, char **argv) {
 	SDL_Renderer *renderer;
 	SDL_Window *window;
 	SDL_Texture *texture;
-	int delay = 100;
 	
 	(void)argc;
 	(void)argv;	
 	
-	
-	if(!init(&renderer, &window, &texture)) {
+	if(init(&renderer, &window, &texture)) {
 		return EXIT_FAILURE;
 	}
 	
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	displayFrame(renderer, texture);
 	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
 }
 
 int init(SDL_Renderer **renderer, SDL_Window **window, SDL_Texture **texture) {
-	int x = 100, y = 100, height = 300, width = 500;
+	int x = 500, y = 300, height = 300, width = 500;
 	
 	if(SDL_Init(0)) {
 		fputs("erreur chargement de la SDL\n", stderr);
@@ -78,5 +78,39 @@ SDL_Texture* loadTextureFromImage(char *name, SDL_Renderer *renderer) {
 	texture = SDL_CreateTextureFromSurface(renderer, surface);
 	return texture;
 }
-void displayFrame(SDL_Renderer *renderer, int frame);
 
+void displayFrame(SDL_Renderer *renderer, SDL_Texture *texture) {
+	SDL_Rect source = {0}, destination = {0}, window = {0}, card = {0};
+	int n = 30, offset_x, offset_y, i, delay = 2000, speed = 10;
+	float zoom = 2;
+
+	srand(time(0));
+	SDL_GetRendererOutputSize(renderer, &window.w, &window.h);
+	SDL_QueryTexture(texture, NULL, NULL, &source.w, &source.h);
+	offset_x = source.w / 14;
+	offset_y = source.h / 4;
+	card.x = 13 * offset_x;
+	card.y = 1 * offset_y;
+	card.w = offset_x;
+	card.h = offset_y;
+	destination.w = offset_x * zoom;
+	destination.h = offset_y * zoom;
+	destination.y = (window.h - destination.h) / 2;
+	SDL_RenderCopy(renderer, texture, &card, &destination);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(delay);
+	for(i = 0; i < n; ++i) {
+		card.x = (rand() % 14 * offset_x) % source.w;
+		card.y = (rand() % 4 * offset_y) % source.h;
+		printf("x %d, y %d\n", card.x, card.y);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, &card, &destination);
+		SDL_RenderPresent(renderer);
+		printf("delay : %d\n", i * 100 / speed + 100);
+		SDL_Delay(i * 100 / speed + 100);
+	}
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, &card, &destination);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(delay);
+}
--- mAnimation/src/animation.h
+++ mAnimation/src/animation.h
@@ -3,8 +3,9 @@
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 SDL_Texture* loadTextureFromImage(char *name, SDL_Renderer *renderer);
-void displayFrame(SDL_Renderer *renderer, int frame);
+void displayFrame(SDL_Renderer *renderer, SDL_Texture *texture);
 int init(SDL_Renderer **renderer, SDL_Window **window, SDL_Texture **texture);
 
