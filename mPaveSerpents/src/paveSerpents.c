9d9900371a1507383931c45ffb951d3dd511ce16
machandeze <marius.chandeze@etu.uca.fr>
2021-06-17 01:18:45 +0000

    ajout du delai a l'affichage et d'un fond en arriere-plan

--- mPaveSerpents/src/paveSerpents.c
+++ mPaveSerpents/src/paveSerpents.c
@@ -4,12 +4,12 @@ int main(int argc, char **argv) {
 	SDL_Renderer *renderer;
 	SDL_Window *window;
 	SDL_Rect r1, r2, r3;
-	int i, x = 100, y = 100, width = 500, height = 300;
+	int i, x = 100, y = 100, width = 500, height = 300, delay = 500;
 
 	(void)argc;
 	(void)argv;
 	
-	if(!SDL_Init()) {
+	if(SDL_Init(SDL_INIT_VIDEO)) {
 		return EXIT_FAILURE;
 	}
 	window = SDL_CreateWindow("pave de serpents", x, y, width, height, 0);
@@ -38,14 +38,17 @@ int main(int argc, char **argv) {
 	r3.w = width / 10;	
 	r3.x = width / 2;
 	r3.y = 2 * height / 3;
-		
-	for(i = 0; i < 1000; ++i) {
+	
+	for(i = 0; i < 100; ++i) {
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
 		moveSquare1(renderer, &r1);
 		moveSquare2(renderer, &r2);
 		moveSquare3(renderer, &r3);
 		SDL_RenderPresent(renderer);
+		SDL_Delay(delay);
 	}
-	
+	SDL_Delay(1000);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
@@ -56,6 +59,8 @@ void moveSquare1(SDL_Renderer *renderer, SDL_Rect *rect) {
 	
 	SDL_GetRendererOutputSize(renderer, &w, &h);
 	SDL_SetRenderDrawColor(renderer, 50, 0, 0, 255);
+	++(rect->x);
+	++(rect->y);
 	if(rect->y > h) {
 		rect->y = 0;
 	}
@@ -65,5 +70,9 @@ void moveSquare1(SDL_Renderer *renderer, SDL_Rect *rect) {
 	SDL_RenderFillRect(renderer, rect);
 }
 
-void moveSquare2(SDL_Renderer *renderer, SDL_Rect *rect);
-void moveSquare3(SDL_Renderer *renderer, SDL_Rect *rect);
+void moveSquare2(SDL_Renderer *renderer, SDL_Rect *rect) {
+	(void)renderer;
+}
+void moveSquare3(SDL_Renderer *renderer, SDL_Rect *rect) {
+	(void)renderer;
+}
