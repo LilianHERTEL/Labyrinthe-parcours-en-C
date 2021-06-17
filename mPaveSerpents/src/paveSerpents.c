a375c0f5b72a58a00c1b85247507289b9bcd4f5f
machandeze <marius.chandeze@etu.uca.fr>
2021-06-17 00:28:00 +0000

    positionnement des carres et maj des en-tetes

--- mPaveSerpents/src/paveSerpents.c
+++ mPaveSerpents/src/paveSerpents.c
@@ -3,6 +3,7 @@
 int main(int argc, char **argv) {
 	SDL_Renderer *renderer;
 	SDL_Window *window;
+	SDL_Rect r1, r2, r3;
 	int i, x = 100, y = 100, width = 500, height = 300;
 
 	(void)argc;
@@ -11,22 +12,37 @@ int main(int argc, char **argv) {
 	if(!SDL_Init()) {
 		return EXIT_FAILURE;
 	}
-	window = SDL_CreateWindow("pave de serpents", 100, 100, 500, 300, 0);
+	window = SDL_CreateWindow("pave de serpents", x, y, width, height, 0);
 	if(window == NULL) {
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
-	renderer = SDL_CreateRenderer(window, -1 ,SDL_RENDERER_ACCELERATED);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if(renderer == NULL) {
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
 	
+	r1.x = 0;
+	r1.y = 0;
+	r1.h = height / 5;
+	r1.w = width / 10;
+
+	r2.h = height / 5;
+	r2.w = width / 10;	
+	r2.x = width - r2.w;
+	r2.y = 0;
+	
+	r3.h = height / 5;
+	r3.w = width / 10;	
+	r3.x = width / 2;
+	r3.y = 2 * height / 3;
+		
 	for(i = 0; i < 1000; ++i) {
-		moveSquare1(renderer);
-		moveSquare2(renderer);
-		moveSquare3(renderer);
+		moveSquare1(renderer, r1);
+		moveSquare2(renderer, r2);
+		moveSquare3(renderer, r3);
 		SDL_RenderPresent(renderer);
 	}
 	
@@ -35,6 +51,6 @@ int main(int argc, char **argv) {
 	SDL_Quit();
 }
 
-void moveSquare1(SDL_Renderer *renderer);
-void moveSquare2(SDL_Renderer *renderer);
-void moveSquare3(SDL_Renderer *renderer);
+void moveSquare1(SDL_Renderer *renderer, SDL_Rect rect);
+void moveSquare2(SDL_Renderer *renderer, SDL_Rect rect);
+void moveSquare3(SDL_Renderer *renderer, SDL_Rect rect);
--- mPaveSerpents/src/paveSerpents.h
+++ mPaveSerpents/src/paveSerpents.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <SDL2/SDL.h>
 
-void moveSquare1(SDL_Renderer *renderer);
-void moveSquare2(SDL_Renderer *renderer);
-void moveSquare3(SDL_Renderer *renderer);
+void moveSquare1(SDL_Renderer *renderer, SDL_Rect rect);
+void moveSquare2(SDL_Renderer *renderer, SDL_Rect rect);
+void moveSquare3(SDL_Renderer *renderer, SDL_Rect rect);
