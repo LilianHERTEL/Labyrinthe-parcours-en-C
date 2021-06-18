f8fc5feea24f814895663475c9cdfc9f404185c4
machandeze <machandeze@turing.local.isima.fr>
2021-06-18 11:39:14 +0200

    implementation testRecupererClasse

--- partition/src/testpartition.c
+++ partition/src/testpartition.c
@@ -14,12 +14,28 @@ bool testCreer() {
 	for(i = 0; i < N; ++i) {
 		if(test.foret[i] != i || test.hauteur[i] != 1) {
 			code = false;
+			puts("echec creation\n");
 		}
 	}
 	return code;
 }
 
-bool testRecupererClasse();
+bool testRecupererClasse() {
+	partition_t partition;
+	
+	partition = creer(N);
+	if(recuperer_classe(partition, 1) != 1) {
+		puts("echec recuperer classe initiale\n");
+		return false;
+	}
+	partition = fusion(partition, 0, 1);
+	if(recuperer_classe(partition, 1) != 0) {
+		puts("echec recuperer classe apres fusion\n");
+		return false;
+	}
+	return true;
+}
+
 bool testFusion();
 bool testListerClasse();
 bool testListerPartition();
