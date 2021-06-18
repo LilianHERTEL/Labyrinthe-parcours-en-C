674f8e7d65182f893775160d71b79b2c484c466a
machandeze <machandeze@turing.local.isima.fr>
2021-06-18 10:54:10 +0200

    correction du type partition

--- partition/src/partition.h
+++ partition/src/partition.h
@@ -6,14 +6,14 @@ typedef int ensemble_t;
 typedef int classe_t;
 typedef int element_t;
 typedef int label_t;
-typedef partition_t {
-	classe_t foret[N],
-	int hauteur[N]
-};
+typedef struct {
+	classe_t foret[N];
+	int hauteur[N];
+} partition_t;
 
 partition_t creer(ensemble_t ensemble);
 classe_t recuperer_classe(partition_t partition, element_t element);
-partition_t fusion(element_t x, element_t y);
-element_t* lister_classe(label_t label, partition_t);
+partition_t fusion(partition_t partition, classe_t x, classe_t y);
+element_t* lister_classe(partition_t partition, label_t label);
 classe_t* lister_partition(partition_t partition);
 
