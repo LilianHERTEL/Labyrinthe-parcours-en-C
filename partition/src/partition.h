10b538e8cc53a0652f210618c50365f55a56b7af
machandeze <machandeze@turing.local.isima.fr>
2021-06-18 09:56:21 +0200

    definition des fonctions

--- partition/src/partition.h
+++ partition/src/partition.h
@@ -1,6 +1,17 @@
 #pragma once
 #include "arbre.h"
 
-
 #define N 5
+
+typedef ensemble_t;
+typedef classe_t;
+typedef partition_t;
+typedef element_t;
+typedef label_t;
+
+partition_t creer(ensemble_t ensemble);
+classe_t recuperer_classe(element_t element);
+partition_t fusion(element_t x, element_t y);
+element_t* lister_classe(label_t label, partition_t);
+classe_t* lister_partition(partition_t partition);
 
