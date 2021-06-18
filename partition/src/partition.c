6f9bee6ac043dc93ee23784c4a90ae9b31a95d7b
machandeze <machandeze@turing.local.isima.fr>
2021-06-18 10:54:32 +0200

    implementation de la fusion

--- partition/src/partition.c
+++ partition/src/partition.c
@@ -11,11 +11,25 @@ partition_t creer(ensemble_t ensemble) {
 	return partition;
 }
 
-classe_t recuperer_classe(element_t element) {
+classe_t recuperer_classe(partition_t partition, element_t element) {
 	return partition.foret[(int) element];
 }
 
-partition_t fusion(element_t x, element_t y);
-element_t* lister_classe(label_t label, partition_t);
-classe_t* lister_partition(partition_t partition);
+partition_t fusion(partition_t partition, classe_t x, classe_t y) {
+	int i;
+	classe_t oldclasse = x, newclasse = y;
+	
+	if(partition.hauteur[(int) y] < partition.hauteur[(int) x]) {
+		oldclasse = y;
+		newclasse = x;
+	}
+	for(i = 0; i < N; ++i) {
+		if(partition.foret[i] == (int) oldclasse) {
+			partition.foret[i] = (int) newclasse;
+		}
+	}
+	return partition;
+}
 
+element_t* lister_classe(partition_t partition, label_t label);
+classe_t* lister_partition(partition_t partition);
