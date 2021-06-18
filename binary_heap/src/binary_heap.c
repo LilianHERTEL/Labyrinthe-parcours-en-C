98a3922f04ec6aa3d0bcdda0682b81a143614592
Lilian <lilian@LilianDebianVM>
2021-06-18 11:55:52 +0200

    heap insert and print OK

--- binary_heap/src/binary_heap.c
+++ binary_heap/src/binary_heap.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "binary_heap.h"
+#include "bool.h"
 
 int getParent(int index) {
     return index/2;
@@ -10,4 +12,60 @@ int getLeftChild(int index) {
 
 int getRightChild(int index) {
     return 2*index + 1;
+}
+
+void printHeap(binary_heap_t heap) {
+    int i;
+    printf("heap size = %d\n", heap.heapSize);
+    for (i = 1; i < heap.heapSize; i++)
+    {
+        printf("%d ", heap.array[i]);
+    }
+    printf("\n");
+}
+
+bool_t heapInsert(binary_heap_t* heap, int val) {
+    bool_t status = false;
+    int index, parent;
+    index = heap->heapSize;
+    if (index < heap->length)
+    {
+        heap->array[index] = val;
+        heap->heapSize++;
+        parent = getParent(index);
+        while (heap->array[index] < heap->array[parent] && index != 1)
+        {
+            permute(heap->array, parent, index);
+            index = parent;
+            parent = getParent(index);
+        }
+        status = true;
+    }
+    return status;
+}
+
+void permute(int array[], int i, int j) {
+    int tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+}
+
+int main(int argc, char const *argv[])
+{
+    binary_heap_t heap;
+    heap.heapSize = 1;
+    heap.length = MAX;
+    bool_t status;
+    status = heapInsert(&heap, 16);
+    status = heapInsert(&heap, 14);
+    status = heapInsert(&heap, 10);
+    status = heapInsert(&heap, 8);
+    status = heapInsert(&heap, 7);
+    status = heapInsert(&heap, 9);
+    status = heapInsert(&heap, 3);
+    status = heapInsert(&heap, 2);
+    status = heapInsert(&heap, 4);
+    status = heapInsert(&heap, 1);
+    printHeap(heap);
+    return 0;
 }
--- binary_heap/src/binary_heap.h
+++ binary_heap/src/binary_heap.h
@@ -1,13 +1,22 @@
 #pragma once
 #include <stdio.h>
+#include "bool.h"
+
+#define MAX 100
 
 typedef struct binary_heap 
 {
     int length;
     int heapSize;
-    int* array;
+    int array[MAX];
 } binary_heap_t;
 
 int getParent(int index);
 int getLeftChild(int index);
 int getRightChild(int index);
+
+void printHeap(binary_heap_t heap);
+bool_t heapInsert(binary_heap_t* heap, int val);
+void permute(int array[], int i, int j);
+//void minHeapify(int array[], int index);
+//void buildMinHeap(binary_heap_t* heap);
