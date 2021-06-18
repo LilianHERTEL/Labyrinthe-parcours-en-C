fa3f15137a9633d6dc5da8e06ca5c79058d6cfc5
machandeze <machandeze@turing.local.isima.fr>
2021-06-18 10:34:55 +0200

    implementation de creer

--- partition/src/partition.c
+++ partition/src/partition.c
@@ -1,3 +1,18 @@
 #include "partition.h"
 
+partition_t creer(ensemble_t ensemble) {
+	partition_t partition;
+	int i;
+	
+	for(i = 0; i < (int) ensemble; ++i) {
+		partition.foret[i] = i;
+		partition.hauteur[i] = 1;
+	}
+	return partition;
+}
+
+classe_t recuperer_classe(element_t element);
+partition_t fusion(element_t x, element_t y);
+element_t* lister_classe(label_t label, partition_t);
+classe_t* lister_partition(partition_t partition);
 
