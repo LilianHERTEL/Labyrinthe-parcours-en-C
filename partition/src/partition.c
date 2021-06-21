728a402c63f17374c1bdb7d02277836aae16a03b
machandeze <machandeze@turing.local.isima.fr>
2021-06-21 11:47:54 +0200

    maj utilisation de n

--- partition/src/partition.c
+++ partition/src/partition.c
@@ -1,9 +1,15 @@
 #include "partition.h"
 
-partition_t creer(ensemble_t ensemble) {
+partition_t creer(ensemble_t ensemble, int n) {
 	partition_t partition;
 	int i;
 	
+	partition.foret = malloc(sizeof(classe_t) * n);
+	partition.hauteur = malloc(sizeof(int) * n);
+	if(partition.hauteur == NULL || partition.foret == NULL) {
+		return partition;
+	}
+
 	for(i = 0; i < (int) ensemble; ++i) {
 		partition.foret[i] = i;
 		partition.hauteur[i] = 1;
@@ -36,43 +42,43 @@ partition_t fusion(partition_t partition, element_t x, element_t y) {
 	return partition;
 }
 
-element_t* lister_classe(partition_t partition, label_t label) {
-	int i, k = 0;;
+element_t* lister_classe(partition_t partition, label_t label, int n) {
+	int i, k = 0;
 	element_t *liste;
 	
-	liste = malloc(sizeof(element_t) * N);
+	liste = malloc(sizeof(element_t) * n);
 	if(liste == NULL) {
 		return NULL;
 	}
 	
-	for(i = 0; i < N; ++i) {
+	for(i = 0; i < n; ++i) {
 		if((int) partition.foret[i] == (int) label) {
 			liste[k] = (element_t) i;
 			++k;
 		}
 	}
-	if(k < N) {
+	if(k < n) {
 		liste[k] = -1;
 	}
 	return liste;
 }
 
-classe_t* lister_partition(partition_t partition) {
+classe_t* lister_partition(partition_t partition, int n) {
 	int i, k;
 	classe_t *liste;
 	
-	liste = malloc(sizeof(classe_t) * N);
+	liste = malloc(sizeof(classe_t) * n);
 	if(liste == NULL) {
 		return NULL;
 	}
 
-	for(i = 0; i < N; ++i) {
+	for(i = 0; i < n; ++i) {
 		if((int) partition.foret[i] == i) {
 			liste[k] = (classe_t) partition.foret[i];
 			++k;
 		}
 	}
-	if(k < N) {
+	if(k < n) {
 		liste[k] = -1;
 	}
 	return liste;
--- partition/src/partition.h
+++ partition/src/partition.h
@@ -2,19 +2,19 @@
 
 #include <stdlib.h>
 
-#define N 11
+//#define N 11
 
 typedef int ensemble_t;
 typedef int classe_t;
 typedef int element_t;
 typedef int label_t;
 typedef struct {
-	classe_t foret[N];
-	int hauteur[N];
+	classe_t *foret;
+	int *hauteur;
 } partition_t;
 
-partition_t creer(ensemble_t ensemble);
+partition_t creer(ensemble_t ensemble, int n);
 classe_t recuperer_classe(partition_t partition, element_t element);
 partition_t fusion(partition_t partition, element_t x, element_t y);
-element_t* lister_classe(partition_t partition, label_t label);
-classe_t* lister_partition(partition_t partition);
+element_t* lister_classe(partition_t partition, label_t labeli, int n);
+classe_t* lister_partition(partition_t partition, int n);
