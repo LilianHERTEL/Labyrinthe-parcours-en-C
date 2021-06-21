087c3be719bfe904e04ea3d2ce316209c3be7b1f
Lilian <lilian@LilianDebianVM>
2021-06-21 15:18:15 +0200

    Adjency matrix prototype fix

--- compo_connexes/src/compoConnexes.c
+++ compo_connexes/src/compoConnexes.c
@@ -1,17 +1,17 @@
 #include "compoConnexes.h"
 
-void genererMatriceAdjacence(graphe_t *graphe) {
+void genererMatriceAdjacence(graphe_t *graphe, int n) {
 	int i, j;
 
 	srand(time(0));
-	graphe = malloc(sizeof(int) * N * N);
+	graphe = malloc(sizeof(int) * n * n);
 	if(graphe == NULL) {
 		return NULL;
 	}
 
-	for(i = 0; i < N; ++i) {
-		for(j = i; j < N; ++j) {
-			graphe[N * i + j] = rand() % 2;
+	for(i = 0; i < n; ++i) {
+		for(j = i; j < n; ++j) {
+			graphe[n * i + j] = rand() % 2;
 		}
 	}
 	return graphe;
--- compo_connexes/src/compoConnexes.h
+++ compo_connexes/src/compoConnexes.h
@@ -5,7 +5,7 @@
 
 typedef int **graphe_t;
 
-void genererMatriceAdjacence(graphe_t *graphe);
+void genererMatriceAdjacence(graphe_t *graphe, int n);
 void afficherGraphe(graphe_t graphe);
 element_t** noeudsCompoConnexes(graphe_t graphe);
 void afficherSousGraphe(graphe_t graphe, element_t* sgraphe);
