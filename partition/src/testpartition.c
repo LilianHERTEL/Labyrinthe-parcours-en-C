a00e2f4b61afca872bfc6e045cee72747e289eab
machandeze <machandeze@turing.local.isima.fr>
2021-06-21 14:17:18 +0200

    correction des fuits de memoire et du test de listerPartitionw

--- partition/src/testpartition.c
+++ partition/src/testpartition.c
@@ -18,6 +18,7 @@ bool testCreer() {
 		}
 	}
 	puts("creer OK");
+	detruirePartition(&test);
 	return code;
 }
 
@@ -26,16 +27,23 @@ bool testRecupererClasse() {
 	int n = 12;
 	
 	partition = creer(n);
+	if(partition.foret == NULL || partition.hauteur == NULL) {
+		fputs("erreur memoire\n", stderr);
+		return false;
+	}
 	if(recuperer_classe(partition, 1) != 1) {
 		puts("echec recuperer classe initiale\n");
+		detruirePartition(&partition);
 		return false;
 	}
 	partition = fusion(partition, 0, 1);
 	if(recuperer_classe(partition, 1) != 0) {
 		puts("echec recuperer classe apres fusion\n");
 		printf("classe de 0 : %d\nclasse de 1 : %d\n", (int) recuperer_classe(partition, 0), (int) recuperer_classe(partition, 1));
+		detruirePartition(&partition);
 		return false;
 	}
+	detruirePartition(&partition);
 	puts("recuperer classe OK");
 	return true;
 }
@@ -45,9 +53,14 @@ bool testFusion() {
 	int n = 11;
 	
 	partition = creer(n);
+	if(partition.foret == NULL || partition.hauteur == NULL) {
+		fputs("erreur memoire\n", stderr);
+		return false;
+	}
 	partition = fusion(partition, 0, 1);
 	if(recuperer_classe(partition, 1) != 0) {
 		puts("echec fusion simple\n");
+		detruirePartition(&partition);
 		return false;
 	}
 	partition = fusion(partition, 2, 3);
@@ -55,6 +68,7 @@ bool testFusion() {
 	if(recuperer_classe(partition, 10) != 2) {
 		puts("echec fusions combinees\n");
 		afficherForet(partition);
+		detruirePartition(&partition);
 		return false;
 	}
 	partition = fusion(partition, 5, 9);
@@ -63,21 +77,25 @@ bool testFusion() {
 	if(recuperer_classe(partition, 8) != 7) {
 		puts("echec fusions combinees\n");
 		afficherForet(partition);
+		detruirePartition(&partition);
 		return false;
 	}
 	partition = fusion(partition, 8, 9);
 	if(recuperer_classe(partition, 5) != 5 || recuperer_classe(partition, 7) != 5 || recuperer_classe(partition, 9) != 5) {
 		puts("echec fusions combinees\n");
 		afficherForet(partition);
+		detruirePartition(&partition);
 		return false;
 	}
 	partition = fusion(partition, 6, 8);
 	if(recuperer_classe(partition, 4) != 5) {
 		puts("echec fusions combinees\n");
 		afficherForet(partition);
+		detruirePartition(&partition);
 		return false;
 	}
 	puts("fusion OK");
+	detruirePartition(&partition);
 	return true;
 }
 
@@ -87,14 +105,20 @@ bool testListerClasse() {
 	int n = 11;
 	
 	partition = creer(n);
+	if(partition.foret == NULL || partition.hauteur == NULL) {
+		fputs("erreur memoire\n", stderr);
+		return false;
+	}
 	partition = fusion(partition, 0, 1);
 	element = lister_classe(partition, 0, n);
 	if(element[0] != 0 || element[1] != 1 || element[2] != -1) {
 		afficherForet(partition);
 		puts("echec listage classes\n");
+		detruirePartition(&partition);
 		return false;
 	}
 	puts("liste classe OK");
+	detruirePartition(&partition);
 	free(element);
 	return true;
 }
@@ -105,6 +129,10 @@ bool testListerPartition() {
 	int i, n = 11, k = 0;
 	
 	partition = creer(n);
+	if(partition.foret == NULL || partition.hauteur == NULL) {
+		fputs("erreur memoire\n", stderr);
+		return false;
+	}
 	for(i = 1; i < 2 * n / 3; ++i) {
 		partition = fusion(partition, 0, i);
 	}
@@ -112,13 +140,15 @@ bool testListerPartition() {
 	while(k < n && k > -1) {
 		++k;
 	}
-	if(classe[0] != 0 || classe[k] == n) {
+	if(classe[0] != 0 || classe[k] == n - 1) {
 		afficherForet(partition);
 		afficherClasse(classe);
 		puts("echec listage partition\n");
+		detruirePartition(&partition);
 		return false;
 	}
 	puts("liste partition OK");
+	detruirePartition(&partition);
 	free(classe);
 	return true;
 }
