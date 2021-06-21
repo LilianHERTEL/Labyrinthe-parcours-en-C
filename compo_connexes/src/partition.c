710242174a824c587d086633ab63b56d09a566d6
machandeze <machandeze@turing.local.isima.fr>
2021-06-21 15:14:42 +0200

    maj des fichier

--- compo_connexes/src/partition.c
+++ compo_connexes/src/partition.c
@@ -4,6 +4,12 @@ partition_t creer(ensemble_t ensemble) {
 	partition_t partition;
 	int i;
 	
+	partition.foret = malloc(sizeof(classe_t) * ensemble);
+	partition.hauteur = malloc(sizeof(int) * ensemble);
+	if(partition.hauteur == NULL || partition.foret == NULL) {
+		return partition;
+	}
+
 	for(i = 0; i < (int) ensemble; ++i) {
 		partition.foret[i] = i;
 		partition.hauteur[i] = 1;
@@ -23,7 +29,6 @@ partition_t fusion(partition_t partition, element_t x, element_t y) {
 	if(partition.hauteur[(int) classey] == partition.hauteur[(int) classex]) {
 		min = (classex <= classey) ? classex : classey;
 		++(partition.hauteur[min]);
-		//partition.foret[(classex == min) ? classey : classex] = min;
 	}
 	if(partition.hauteur[(int) classey] < partition.hauteur[(int) classex]) {
 		partition.foret[(int) y] = classex;
@@ -36,44 +41,69 @@ partition_t fusion(partition_t partition, element_t x, element_t y) {
 	return partition;
 }
 
-element_t* lister_classe(partition_t partition, label_t label) {
-	int i, k = 0;;
+element_t* lister_classe(partition_t partition, label_t label, int n) {
+	int i, k = 0;
 	element_t *liste;
 	
-	liste = malloc(sizeof(element_t) * N);
+	liste = malloc(sizeof(element_t) * n);
 	if(liste == NULL) {
 		return NULL;
 	}
 	
-	for(i = 0; i < N; ++i) {
+	for(i = 0; i < n; ++i) {
 		if((int) partition.foret[i] == (int) label) {
 			liste[k] = (element_t) i;
 			++k;
 		}
 	}
-	if(k < N) {
+	if(k < n) {
 		liste[k] = -1;
 	}
 	return liste;
 }
 
-classe_t* lister_partition(partition_t partition) {
-	int i, k;
+void detruirePartition(partition_t partition) {
+	free(partition.foret);
+	free(partition.hauteur);
+}
+
+classe_t* lister_partition(partition_t partition, int n) {
+	int i, k = 0;
 	classe_t *liste;
 	
-	liste = malloc(sizeof(classe_t) * N);
+	liste = malloc(sizeof(classe_t) * n);
 	if(liste == NULL) {
 		return NULL;
 	}
 
-	for(i = 0; i < N; ++i) {
+	for(i = 0; i < n; ++i) {
 		if((int) partition.foret[i] == i) {
 			liste[k] = (classe_t) partition.foret[i];
 			++k;
 		}
 	}
-	if(k < N) {
+	if(k < n) {
 		liste[k] = -1;
 	}
 	return liste;
 }
+
+void afficherClasse(classe_t *classe) {
+        int i = 0, n = 11;
+
+        while(i < n && classe[i] != -1) {
+                printf("%d ", classe[i]);
+                ++i;
+        }
+        puts("");
+}
+
+void afficherForet(partition_t partition) {
+        int i, n = 11;
+
+        for(i = 0; i < n; ++i) {
+                printf("indice[%d] : %d (hauteur %d)\n", i, partition.foret[i], partition.hauteur[i]);
+        }
+        puts("");
+}
+
--- partition/src/graphviz.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "partition.h"
-#include <graphviz/gvc.h>
-#include <graphviz/cgraph.h>
-
-void partitionSimple();
