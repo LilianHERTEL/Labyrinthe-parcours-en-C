fb7d112f60b65f6c4b928b42f3a3a0ae51f983a8
machandeze <machandeze@turing.local.isima.fr>
2021-06-21 16:40:30 +0200

    implementation de noeudsCompoConnexes

--- compo_connexes/src/compoConnexes.c
+++ compo_connexes/src/compoConnexes.c
@@ -28,6 +28,29 @@ void afficherMatriceAdjacence(graphe_t graphe, int n) {
 	}
 }
 
+element_t** noeudsCompoConnexes(graphe_t graphe, int n) {
+	partition_t partition;
+	element_t **noeud;
+	classe_t *classe;
+	int i = 0, j;
+
+	partition = grapheToPartition(graphe);
+	classe = (element_t *)lister_partition(partition, n);
+	while(i < n && classe[i] > -1) {
+		//c[i] = lister_classe(partition, i, n);
+		++i;
+	}
+	noeud = malloc(sizeof(element_t *) * i);
+	if(noeud == NULL) {
+		fputs("erreur allocation de memoire noeudcompoconnexe\n", stderr);
+		return NULL;
+	}
+	for(j = 0; j < i; ++j) {
+		noeud[j] = lister_classe(partition, i, n);
+	}
+	return noeud;
+}
+
 int main(int argc, char const *argv[])
 {
 	graphe_t graph;
--- compo_connexes/src/compoConnexes.h
+++ compo_connexes/src/compoConnexes.h
@@ -8,5 +8,6 @@ typedef int **graphe_t;
 void genererMatriceAdjacence(graphe_t *graphe, int n);
 void afficherMatriceAdjacence(graphe_t graphe, int n);
 void afficherGraphe(graphe_t graphe);
-element_t** noeudsCompoConnexes(graphe_t graphe);
+element_t** noeudsCompoConnexes(graphe_t graphe, int n);
 void afficherSousGraphe(graphe_t graphe, element_t* sgraphe);
+partition_t grapheToPartition(graphe_t graphe);
