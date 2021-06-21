f88ce58b0341cd1450c9f34ffce73c6291597e17
Lilian <lilian@LilianDebianVM>
2021-06-21 09:28:49 +0200

    Fixed

---
+++ binary_heap/src/compar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "binary_heap.h"
+
+#define SIZE 1000
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    time_t t;
+    binary_heap_t heap;
+
+    srand((unsigned)time(&t));
+    for (i = 0 ; i < SIZE ; i++) {
+        printf("% d \n", rand() % SIZE);
+    }
+
+    return 0;
+}
