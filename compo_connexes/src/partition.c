9eea55659043e8b0df56a73e1371ad9ffbf42e2f
machandeze <machandeze@turing.local.isima.fr>
2021-06-21 11:01:34 +0200

    ajout module partition

---
+++ compo_connexes/src/partition.c
@@ -0,0 +1,79 @@
+#include "partition.h"
+
+partition_t creer(ensemble_t ensemble) {
+	partition_t partition;
+	int i;
+	
+	for(i = 0; i < (int) ensemble; ++i) {
+		partition.foret[i] = i;
+		partition.hauteur[i] = 1;
+	}
+	return partition;
+}
+
+classe_t recuperer_classe(partition_t partition, element_t element) {
+	return partition.foret[(int) element];
+}
+
+partition_t fusion(partition_t partition, element_t x, element_t y) {
+	classe_t classex, classey, min;
+	
+	classex = recuperer_classe(partition, x);
+	classey = recuperer_classe(partition, y);
+	if(partition.hauteur[(int) classey] == partition.hauteur[(int) classex]) {
+		min = (classex <= classey) ? classex : classey;
+		++(partition.hauteur[min]);
+		//partition.foret[(classex == min) ? classey : classex] = min;
+	}
+	if(partition.hauteur[(int) classey] < partition.hauteur[(int) classex]) {
+		partition.foret[(int) y] = classex;
+		partition.foret[(int) classey] = classex;
+	}
+	else {
+		partition.foret[(int) x] = classey;
+		partition.foret[(int) classex] = classey;
+	}
+	return partition;
+}
+
+element_t* lister_classe(partition_t partition, label_t label) {
+	int i, k = 0;;
+	element_t *liste;
+	
+	liste = malloc(sizeof(element_t) * N);
+	if(liste == NULL) {
+		return NULL;
+	}
+	
+	for(i = 0; i < N; ++i) {
+		if((int) partition.foret[i] == (int) label) {
+			liste[k] = (element_t) i;
+			++k;
+		}
+	}
+	if(k < N) {
+		liste[k] = -1;
+	}
+	return liste;
+}
+
+classe_t* lister_partition(partition_t partition) {
+	int i, k;
+	classe_t *liste;
+	
+	liste = malloc(sizeof(classe_t) * N);
+	if(liste == NULL) {
+		return NULL;
+	}
+
+	for(i = 0; i < N; ++i) {
+		if((int) partition.foret[i] == i) {
+			liste[k] = (classe_t) partition.foret[i];
+			++k;
+		}
+	}
+	if(k < N) {
+		liste[k] = -1;
+	}
+	return liste;
+}
---
+++ compo_connexes/src/partition.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdlib.h>
+
+#define N 11
+
+typedef int ensemble_t;
+typedef int classe_t;
+typedef int element_t;
+typedef int label_t;
+typedef struct {
+	classe_t foret[N];
+	int hauteur[N];
+} partition_t;
+
+partition_t creer(ensemble_t ensemble);
+classe_t recuperer_classe(partition_t partition, element_t element);
+partition_t fusion(partition_t partition, element_t x, element_t y);
+element_t* lister_classe(partition_t partition, label_t label);
+classe_t* lister_partition(partition_t partition);
