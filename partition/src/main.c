feec5c5dddcc8ebc036831d4c16dc03554f09b78
kimeunier <killian.meunier@etu.uca.fr>
2021-06-21 16:08:08 +0200

    ajout d'un test dans le main

--- partition/src/main.c
+++ partition/src/main.c
@@ -7,7 +7,19 @@ int main(int argc, char *argv[])
     (void)argv;
 
     (testCreer() && testRecupererClasse() && testFusion() && testListerClasse() && testListerPartition()) ? puts("tests ok") : puts("erreur tests");
-    
+
+    partition_t part;
+    int         n;
+
+    n = 10;
+    part = creer(n);
+    fusion(part, part.foret[0], part.foret[2]);
+    fusion(part, part.foret[0], part.foret[8]);
+    fusion(part, part.foret[3], part.foret[7]);
+
+    drawPartitionGraph(part, n, "exemplePartition");
+
+    detruirePartition(part);
 
     return 0;
 }
