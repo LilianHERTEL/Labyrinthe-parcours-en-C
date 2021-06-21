d2647a5e7fb4d8ea5dcd85854cb08ecdb3c5f06f
machandeze <machandeze@turing.local.isima.fr>
2021-06-21 14:27:26 +0200

    merge

--- partition/src/graphviz.c
+++ partition/src/graphviz.c
@@ -2,24 +2,30 @@
 
 void partitionSimple()
 {
-	partition_t   test;
+    partition_t   test;
     Agraph_t    * graphe;
-    Agnode_t    * noeuds[N];
+    Agnode_t   ** noeuds;
     FILE        * fic;
     GVC_t       * graph_context;
     int           i,
                   sys;
     char          nom[2];
+    int 	  n = 11;
 
+    noeuds = malloc(sizeof(Agnode_t *) * n);
+    if(noeuds == NULL) {
+	fputs("erreur allocation de memoire pour les neouds\n", stderr);
+        return;
+    }
     fic = fopen("partitionSimple.dot", "w");
     if(fic)
     {
-        test = creer(N);
+        test = creer(n);
         graph_context = gvContext();
         if (graph_context)
         {
             graphe = agopen("PartitionSimple", Agdirected, 0);
-            for(i = 0; i<N; i++)
+            for(i = 0; i<n; i++)
             {
                 sprintf(nom, "%d", test.foret[i]);
                 noeuds[i] = agnode(graphe, nom, 1);
