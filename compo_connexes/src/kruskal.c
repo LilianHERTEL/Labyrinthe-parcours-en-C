0f2886c2ad40d6b3705183391c1a947e8b9b45e0
kimeunier <killian.meunier@etu.uca.fr>
2021-06-22 12:08:17 +0200

    ajout algo de kruskal

--- compo_connexes/src/kruskal.c
+++ compo_connexes/src/kruskal.c
@@ -2,10 +2,33 @@
 
 arete_t * arbreCouvrantPoidsMin(couples_graphe_t graph)
 {
-    arete_t * listeAretes;
+    arete_t       * listeAretes;
+    partition_t     partition;
+    classe_t        classe1,
+                    classe2;
+    element_t       elem1,
+                    elem2;
+    int             i,
+                    j = 0;
 
-    ordonnerAretesCroissant(&graph);
+    listeAretes = (arete_t *)malloc(sizeof(arete_t) * graph.nbAretes);
+    if(listeAretes)
+    {
+        ordonnerAretesCroissant(&graph);
+        partition = creer(graph.nbNoeuds);
 
+        for(i = 0; i < graph.nbAretes; i++)
+        {
+            elem1 = graph.aretes[i].noeudDeb;
+            elem2 = graph.aretes[i].noeudFin;
+            if(classeMere(elem1, partition) != classeMere(elem2, partition))
+            {   
+                fusion(partition, elem1, elem2);
+                listeAretes[j] = graph.aretes[i];
+                j++;
+            }
+        }
+    }
     
     return listeAretes;
 }
