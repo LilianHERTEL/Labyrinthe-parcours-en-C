567b126c8d7cd2af59feaac583bbf56005e5f3f9
kimeunier <killian.meunier@etu.uca.fr>
2021-06-24 10:30:09 +0200

    ajout du parcours en profondeur sans nombre de voisins

--- dijkstra/src/parcours.c
+++ dijkstra/src/parcours.c
@@ -1,6 +1,7 @@
 #include "parcours.h"
 
-bool_t dijkstra(couples_graphe_t graphe, int source, int cible, liste_t *chemin, int n) {
+bool_t dijkstra(couples_graphe_t graphe, int source, int cible, liste_t *chemin, int n)
+{
 	binary_heap_t tas;
 	bool_t found = false;
 	node_t cour, noeudVoisin;
@@ -12,7 +13,8 @@ bool_t dijkstra(couples_graphe_t graphe, int source, int cible, liste_t *chemin,
 	tas.array = malloc(sizeof(int) * n);
 	prec = malloc(sizeof(int) * n);
 	*chemin = initialisation_liste();
-	if(tas.array == NULL || prec == NULL) {
+	if (tas.array == NULL || prec == NULL)
+	{
 		fputs("erreur malloc dijkstra\n", stderr);
 		return false;
 	}
@@ -38,38 +40,45 @@ bool_t dijkstra(couples_graphe_t graphe, int source, int cible, liste_t *chemin,
 	minHeapInsert(&tas, cour);
 
 	//boucle principale tant qu'on a pas trouve et que le tas n'est pas vide
-	while(found == false && tas.heapSize != 0) {
-		
+	while (found == false && tas.heapSize != 0)
+	{
+
 		//on prend le premier de la file d'attente
-        cour = heapExtractMin(&tas);
-        fprintf(stderr, "on passe a %d de distance %f\n", cour.num, cour.dist);
-        //printHeap(tas);
-        if(cour.num == -1) {
-            puts("erreur");
-            free(prec);
-            free(tas.array);
-            return false;
-        }
+		cour = heapExtractMin(&tas);
+		fprintf(stderr, "on passe a %d de distance %f\n", cour.num, cour.dist);
+		//printHeap(tas);
+		if (cour.num == -1)
+		{
+			puts("erreur");
+			free(prec);
+			free(tas.array);
+			return false;
+		}
 		//tant qu'on n'a pas atteint la cible
-		if(cour.num != cible) {
-            puts("cible non atteinte\n");
+		if (cour.num != cible)
+		{
+			puts("cible non atteinte\n");
 
 			//pour tous les voisins du noeud courant
-			for(i = 0; i < n; ++i) {
-				if(graphe.aretes[i].noeudDeb == cour.num) {
-                    fprintf(stderr, "on check %d -> %d\n", graphe.aretes[i].noeudDeb, graphe.aretes[i].noeudFin);
+			for (i = 0; i < n; ++i)
+			{
+				if (graphe.aretes[i].noeudDeb == cour.num)
+				{
+					fprintf(stderr, "on check %d -> %d\n", graphe.aretes[i].noeudDeb, graphe.aretes[i].noeudFin);
 					/*
 					 * si on met tous les voisins au debut, pas besoin de verifier
 					 */
 					//si le voisin est dans le tas
-					if(isInHeap(tas, graphe.aretes[i].noeudFin, &voisin)) {
-                        puts("voisin dans le tas");
-					
+					if (isInHeap(tas, graphe.aretes[i].noeudFin, &voisin))
+					{
+						puts("voisin dans le tas");
+
 						//on calcule la distance de la source au voisin en passant par le noeud courant
 						newdist = graphe.aretes[i].poids + cour.dist;
 
 						//si elle est plus courte que la distance precedente, alors on la remplace
-						if(tas.array[voisin].dist > newdist) {
+						if (tas.array[voisin].dist > newdist)
+						{
 
 							//on definit le noeud courant en tant que precedent du voisin
 							prec[tas.array[voisin].num] = cour.num;
@@ -80,51 +89,59 @@ bool_t dijkstra(couples_graphe_t graphe, int source, int cible, liste_t *chemin,
 					}
 
 					//si le voisin n'est pas dans le tas
-					else {
-                        if(graphe.aretes[i].noeudFin == cour.num) {
-                            fprintf(stderr, "on check %d <- %d\n", graphe.aretes[i].noeudDeb, graphe.aretes[i].noeudFin);
-        					//si le voisin est dans le tas
-        					if(isInHeap(tas, graphe.aretes[i].noeudDeb, &voisin)) {
-                                puts("voisin dans le tas");
-					
-				        		//on calcule la distance de la source au voisin en passant par le noeud courant
-        						newdist = graphe.aretes[i].poids + cour.dist;
+					else
+					{
+						if (graphe.aretes[i].noeudFin == cour.num)
+						{
+							fprintf(stderr, "on check %d <- %d\n", graphe.aretes[i].noeudDeb, graphe.aretes[i].noeudFin);
+							//si le voisin est dans le tas
+							if (isInHeap(tas, graphe.aretes[i].noeudDeb, &voisin))
+							{
+								puts("voisin dans le tas");
 
-		        				//si elle est plus courte que la distance precedente, alors on la remplace
-				        		if(tas.array[voisin].dist > newdist) {
+								//on calcule la distance de la source au voisin en passant par le noeud courant
+								newdist = graphe.aretes[i].poids + cour.dist;
 
-						        	//on definit le noeud courant en tant que precedent du voisin
-        							prec[tas.array[voisin].num] = cour.num;
+								//si elle est plus courte que la distance precedente, alors on la remplace
+								if (tas.array[voisin].dist > newdist)
+								{
 
-		        					//on met a jour sa distance
-				        			tas.array[voisin].dist = newdist;
-						        }
-        					}
-                        }
-                        else {
-                            puts("voisin pas dans le tas");
-		    				//ajouter le voisin au tas
-			    			fprintf(stderr, "on ajoute %d au tas", graphe.aretes[i].noeudFin);
-				    		noeudVoisin.num = graphe.aretes[i].noeudFin;
-					    	noeudVoisin.dist = graphe.aretes[i].poids + cour.dist;
-						    minHeapInsert(&tas, noeudVoisin);
-                        }
+									//on definit le noeud courant en tant que precedent du voisin
+									prec[tas.array[voisin].num] = cour.num;
+
+									//on met a jour sa distance
+									tas.array[voisin].dist = newdist;
+								}
+							}
+						}
+						else
+						{
+							puts("voisin pas dans le tas");
+							//ajouter le voisin au tas
+							fprintf(stderr, "on ajoute %d au tas", graphe.aretes[i].noeudFin);
+							noeudVoisin.num = graphe.aretes[i].noeudFin;
+							noeudVoisin.dist = graphe.aretes[i].poids + cour.dist;
+							minHeapInsert(&tas, noeudVoisin);
+						}
 					}
 				}
 			}
 		}
-		else {
-            puts("cible trouvee");
+		else
+		{
+			puts("cible trouvee");
 			//cible trouvee
 			found = true;
 		}
-        puts("\n");
+		puts("\n");
 	}
 
 	//on remonte le tableau des precedents en partant de la cible
 	u = cible;
-	if((prec[u] != -1 || u == source) && found == true) {
-		while(prec[u] != -1) {
+	if ((prec[u] != -1 || u == source) && found == true)
+	{
+		while (prec[u] != -1)
+		{
 			//on ajoute le numero courant
 			//chemin[k] = prec[u];
 			printf("u : %d\n", u);
@@ -134,27 +151,28 @@ bool_t dijkstra(couples_graphe_t graphe, int source, int cible, liste_t *chemin,
 			//++k;
 		}
 	}
-    else {
-        fputs("cible pas trouvee\n", stderr);
-	    free(prec);
-    	free(tas.array);
-        return false;
-    }
+	else
+	{
+		fputs("cible pas trouvee\n", stderr);
+		free(prec);
+		free(tas.array);
+		return false;
+	}
 
 	free(prec);
 	free(tas.array);
 	return found;
 }
 
-void parcoursEnProfondeur(couples_graphe_t graph, int debut)
+void parcoursEnProfondeur(couples_graphe_t graph, int debut, SDL_Renderer *renderer, int n, int m, SDL_Rect tile, SDL_Rect positionLab, SDL_Texture *texture, int **grille, SDL_Rect destPerso, SDL_Texture *perso)
 {
-	bool_t * marques;
+	bool_t *marques;
 
 	marques = (bool_t *)malloc(sizeof(bool_t) * graph.nbNoeuds);
-	if(marques)
+	if (marques)
 	{
 		initialiserMarque(marques, graph.nbNoeuds);
-		explorer(graph, debut, marques);
+		explorer(graph, debut, marques, renderer, n, m, tile, positionLab, texture, grille, destPerso, perso);
 	}
 	else
 	{
@@ -162,30 +180,40 @@ void parcoursEnProfondeur(couples_graphe_t graph, int debut)
 	}
 }
 
-void initialiserMarque(bool_t * marques, int n)
+void initialiserMarque(bool_t *marques, int n)
 {
 	int i;
 
-	for(i = 0; i<n; i++)
+	for (i = 0; i < n; i++)
 	{
 		marques[i] = false;
 	}
 }
 
-void explorer(couples_graphe_t graph, int s, bool_t * marques)
+void explorer(couples_graphe_t graph, int s, bool_t *marques, SDL_Renderer *renderer, int n, int m, SDL_Rect tile, SDL_Rect positionLab, SDL_Texture *texture, int **grille, SDL_Rect destPerso, SDL_Texture *perso)
 {
-	int * voisins,
-		  nbVoisins,
-		  i;
+	int *voisins,
+		nbVoisins,
+		i;
 
 	marques[s] = true;
+
 	//affichage
-	voisins = NULL;//trouverVoisins(graph, s, &nbVoisins);
-	for(i = 0; i < nbVoisins; i++)
+	SDL_RenderClear(renderer);
+	drawLab(renderer, grille, n, m, tile, positionLab, texture);
+	drawChemin(renderer, n, m, tile, positionLab, texture, marques, graph.nbNoeuds);
+	destPerso.x = positionLab.x + tile.w * (s - s / n * n) + tile.w * 0.1;
+	destPerso.y = positionLab.y + tile.h * (s / n) + tile.h * 0.1;
+	drawperso(renderer, perso, destPerso);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(100);
+
+	voisins = NULL; //trouverVoisins(graph, s, &nbVoisins);
+	for (i = 0; i < nbVoisins; i++)
 	{
-		if(!marques[voisins[i]])
+		if (!marques[voisins[i]])
 		{
-			explorer(graph, voisins[i], marques);
+			explorer(graph, voisins[i], marques, renderer, n, m, tile, positionLab, texture, grille, destPerso, perso);
 		}
-	} 
+	}
 }
--- dijkstra/src/parcours.h
+++ dijkstra/src/parcours.h
@@ -12,8 +12,8 @@
 
 bool_t dijkstra(couples_graphe_t graphe, int source, int cible, liste_t *chemin, int n);
 
-void parcoursEnProfondeur(couples_graphe_t graph, int debut);
+void parcoursEnProfondeur(couples_graphe_t graph, int debut, SDL_Renderer *renderer, int n, int m, SDL_Rect tile, SDL_Rect positionLab, SDL_Texture *texture, int **grille, SDL_Rect destPerso, SDL_Texture *perso);
 
-void initialiserMarque(bool_t * marques, int n);
+void initialiserMarque(bool_t *marques, int n);
 
-void explorer(couples_graphe_t graph, int s, bool_t * marques);
+void explorer(couples_graphe_t graph, int s, bool_t *marques, SDL_Renderer *renderer, int n, int m, SDL_Rect tile, SDL_Rect positionLab, SDL_Texture *texture, int **grille, SDL_Rect destPerso, SDL_Texture *perso);
