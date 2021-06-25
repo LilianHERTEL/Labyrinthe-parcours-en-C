80980ae3ebecc35ddddb61c15ba5a0c2d215035b
kimeunier <killian.meunier@etu.uca.fr>
2021-06-25 10:19:27 +0200

    Documentation

--- compo_connexes/src/compoConnexes.h
+++ compo_connexes/src/compoConnexes.h
@@ -7,40 +7,46 @@
 
 typedef int *graphe_t;
 
-/** 
- * @brief cree un graphe a partir d'un matrice d'adjacence
- * @param n la taille de la matrice
- */
-graphe_t createAdjencyMatrix(int n);
-
-/** 
- * @brief affiche une matrice d'adjacence
- * @param matrix la matrice
- * @param n la taille de la matrice
+/**
+ * @brief Affiche une matrice d'adjacence dans le terminal
+ * 
+ * @param matrix La matrice
+ * @param n La taille de la matrice (carree)
  */
 void afficherMatriceAdjacence(graphe_t matrix, int n);
 
-/** 
- * @brief Calcule les noeuds de chacune des composantes connexes du graphe
- * @param graphe le graphe
- * @param n la taille de la matrice d'adjacence du graphe
- * @param noeudsI le nombre de composantes connexes
- * @return la liste des tableaux d'elements de chaque composante connexe
+/**
+ * @brief Cree une matrice d'adjacence qui represente un graphe
+ * 
+ * @param n La taille de la matrice
+ * @return graphe_t 
  */
-element_t** noeudsCompoConnexes(graphe_t graphe, int n, int * noeudsI);
+graphe_t createAdjencyMatrix(int n);
 
-/** 
- * @brief affiche le sous-graphe specifie d'un graphe
- * @param graphe le graphe
- * @param n la taille de la matrice d'adjacence du graphe
- */
-void afficherSousGraphe(graphe_t graphe, element_t* sgraphe);
-
-/** 
- * @brief pratitionne un graphe en ses composantes connexes
- * @param graphe le graphe
- * @param n la taille de la matrice d'adjacence du graphe
- * @return la partition des composantes connexes
+/**
+ * @brief Partitionne un graphe en ses composantes connexes
+ * 
+ * @param graphe Le graphe
+ * @param n La taille de la matrice d'adjacence du graphe
+ * @return partition_t La partition des composantes connexes
  */
 partition_t grapheToPartition(graphe_t graphe, int n);
-void afficherNoeudsCompoConnexes(element_t ** noeuds, int n_noeuds, int n);
+
+/**
+ * @brief Calcule les noeuds de chacune des composantes connexes du graphe
+ * 
+ * @param graphe Le graphe
+ * @param n La taille de la matrice d'adjacence du graphe
+ * @param noeudsI Le nombre de composantes connexes
+ * @return element_t** La liste des tableaux d'elements de chaque composante connexe
+ */
+element_t **noeudsCompoConnexes(graphe_t graphe, int n, int *noeudsI);
+
+/**
+ * @brief Affiche dans le terminal chaque noeud pour chaque classe
+ * 
+ * @param noeuds Les noeuds de chacunes de composantes connexes
+ * @param n Nombre d'elements max d'une classe
+ * @param nclasse Nombre de classes
+ */
+void afficherNoeudsCompoConnexes(element_t **noeuds, int n_noeuds, int n);
